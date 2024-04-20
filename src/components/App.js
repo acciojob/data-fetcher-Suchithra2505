9839a48861b1fb3d80292a0dd2c17109fbe890fc
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-04-20 05:17:12 +0000

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
 import './../styles/App.css';
+
+const DataFetcher = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+}
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App
